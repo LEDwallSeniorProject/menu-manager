65376355b866c767bbb885c2a95dc250d923863a
alex-j-miller <alexiscool2003@sbcglobal.net>
2024-11-19 16:49:00 -0500

    added more games for presentation

---
+++ games/Clock/main.py
@@ -0,0 +1,62 @@
+from matrix_library import shapes as s, canvas as c
+from datetime import datetime
+import time
+from evdev import InputDevice, categorize, ecodes
+
+gamepad = InputDevice("/dev/input/event1")
+
+# NOTE: This is modified to only run for 10 seconds
+timer = 0
+
+
+def exit_prog():
+    print("quit")
+    canvas.delete()
+    sys.exit()
+
+
+canvas = c.Canvas()
+circle = s.CircleOutline(64, (64, 64), (255, 255, 255))
+
+markers = []
+for i in range(12):
+    marker = s.Line((64, 16), (64, 8), (255, 255, 255))
+    marker.rotate(i * 30, (64, 64))
+    markers.append(marker)
+
+while True:
+
+    if gamepad.active_keys() == [24]:
+        exit_prog()
+
+    canvas.clear()
+
+    hour_hand = s.Line((64, 64), (64, 24), (255, 0, 0))
+    minute_hand = s.Line((64, 64), (64, 20), (0, 255, 0))
+    second_hand = s.Line((64, 64), (64, 16), (0, 0, 255))
+
+    now = datetime.now()
+    hour = now.hour
+    minute = now.minute
+    second = now.second
+    millisecond = now.microsecond / 1000
+
+    hour_angle = (hour % 12) * 30 + (minute / 60) * 30
+    minute_angle = minute * 6 + (second / 60) * 6
+    second_angle = second * 6 + (millisecond / 1000) * 6
+
+    hour_hand.rotate(hour_angle, (64, 64))
+    minute_hand.rotate(minute_angle, (64, 64))
+    second_hand.rotate(second_angle, (64, 64))
+
+    canvas.add(circle)
+
+    for marker in markers:
+        canvas.add(marker)
+
+    canvas.add(hour_hand)
+    canvas.add(minute_hand)
+    canvas.add(second_hand)
+    canvas.draw()
+    time.sleep(1)
+    timer += 1
---
+++ games/Genesis/main.py
@@ -0,0 +1,15 @@
+from matrix_library import shapes as s, canvas as c
+
+canvas = c.Canvas()
+
+text = s.Phrase(
+    "In the beginning, God created the heavens and the earth. The earth was without form and void, and darkness was over the face of the deep. And the Spirit of God was hovering over the face of the waters. And God said, 'Let there be light,' and there was light. And God saw that the light was good. And God separated the light from the darkness. God called the light Day, and the darkness he called Night. And there was evening and there was morning, the first day. And God said, 'Let there be an expanse in the midst of the waters, and let it separate the waters from the waters.' And God made the expanse and separated the waters that were under the expanse from the waters that were above the expanse. And it was so. And God called the expanse Heaven. And there was evening and there was morning, the second day.",
+    [0, 0],
+    auto_newline=True,
+)
+
+for i in range(100):
+    canvas.clear()
+    text.translate(0, -1)
+    canvas.add(text)
+    canvas.draw()
