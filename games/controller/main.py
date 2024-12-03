d2fc58b5adf28389403e5936d6e0250b069a590c
CSLedPI <cpsc-admin@calvin.edu>
2024-12-03 16:07:40 -0500

    more fixes

--- games/controller/main.py
+++ games/controller/main.py
@@ -1,4 +1,6 @@
 import matrix_library as matrix
+import sys
+import time
 
 # Authors: Chris Wieringa, Alex Miller, Ellie Sand, Palmer Ford
 # Purpose: Simple test program for controllers. Shows in STDOUT debug
@@ -42,7 +44,11 @@ polygon = matrix.Polygon(matrix.get_polygon_vertices(5, 20, (64, 64)), (255, 0, 
 # Whether to show the polygon or not
 toggle = True
 
-phrase = matrix.Phrase(text="Key: ",position=[5,115])
+# Global running
+running = True
+
+# Make the Phrase for the canvas
+phrase = matrix.Phrase(text="Key: ",position=[0,105],auto_newline=True)
 
 # Function to bind to a controller button press
 def toggle_toggle_up():
@@ -62,19 +68,30 @@ def toggle_toggle_up2():
     global toggle
     toggle = not toggle
 
+# Function to exit
+def shutdown():
+    print("Pressed button START or START2")
+    global phrase
+    phrase.set_text("Shutdown via START or START2")
+    time.sleep(3)
+
+    global running
+    running = False
         
 # Example binding of a controller presses to a function
 controller.add_function("UP", toggle_toggle_up)
 controller.add_function("UP2", toggle_toggle_up2)
+controller.add_function("START", shutdown)
+controller.add_function("START2", shutdown)
 
 # Make a whole lot of functions programatically for all our other possible buttons
 leftoverbuttons = {
     # controller for player one, use WASD and nearby keps
     "LB": "q", "RB": "e", "DOWN": "s", "LEFT": "a", "RIGHT": "d", 
-    "A": "g", "B": "v", "Y": "f", "X": "t","SELECT": "z", "START": "c",
+    "A": "g", "B": "v", "Y": "f", "X": "t","SELECT": "z",
     # controller for player two, use IJKL and nearby keys
     "LB2": "u", "RB2": "o", "DOWN2": "k", "LEFT2": "j", "RIGHT2": "l",
-    "A2": "'", "B2": "/", "Y2": ";", "X2": "[", "SELECT2": "m", "START2": ".",
+    "A2": "'", "B2": "/", "Y2": ";", "X2": "[", "SELECT2": "m",
 }
 for key, value in leftoverbuttons.items():
     exec(f"def {key}(): \n\tglobal phrase\n\tphrase.set_text(\"Key: {key}\")\n\tprint(\"Pressed button {key}\")")
@@ -87,7 +104,7 @@ for button in leftoverbuttons.keys():
 # MAIN PROGRAM
 
 # Animation loop
-while True:
+while running:
     # clear the canvas
     canvas.clear()
 
